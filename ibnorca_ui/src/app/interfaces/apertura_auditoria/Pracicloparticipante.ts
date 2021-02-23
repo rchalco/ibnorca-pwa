2bd9be132ee01ea86c6ae706bbe58b0c656c228b
mikyches <68037447+mikyches@users.noreply.github.com>
2021-02-23 19:28:23 -0400

    entidades

---
+++ ibnorca_ui/src/app/interfaces/apertura_auditoria/Pracicloparticipante.ts
@@ -0,0 +1,14 @@
+export class Pracicloparticipante {
+    constructor(){
+
+    }
+    idCicloParticipante: number;
+    idPrAcicloProgAuditoria: number | null;
+    idParticipanteWs: string;
+    participanteContextWs: string;
+    idpCargoParticipante: number | null;
+    usuarioRegistro: string;
+    fechaDesde: string | null;
+    idpEstadoParticipante: number | null;
+    idPrAcicloProgAuditoriaNavigation: Praciclosprogauditorium;
+}

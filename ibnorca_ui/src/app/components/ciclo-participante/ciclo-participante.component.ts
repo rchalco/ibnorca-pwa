0a8b6c5426665184392412e214977139d312e90e
mikyches <68037447+mikyches@users.noreply.github.com>
2021-02-25 20:23:51 -0400

    participantes

--- ibnorca_ui/src/app/components/ciclo-participante/ciclo-participante.component.ts
+++ ibnorca_ui/src/app/components/ciclo-participante/ciclo-participante.component.ts
@@ -7,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CicloParticipanteComponent implements OnInit {
 
+  visibleAdd = "NO";
   constructor() { }
+
+  lstParticipantes = [
+    { Cargo: "(AUDITOR LÌDER)", ParticipanteContextWs: "Ruben Chalco", FechaDesde: "" },
+    { Cargo: "(AUDITOR 1)", ParticipanteContextWs: "Ivan Vilela", FechaDesde: "" },
+    { Cargo: "(AUDITOR 2)", ParticipanteContextWs: "Juan Perez", FechaDesde: "" },
+    { Cargo: "(AUDITOR 3)", ParticipanteContextWs: "Ximena Prada", FechaDesde: "" },
+    { Cargo: "(AUDITOR ENSAYOS CON TESTIGO)", ParticipanteContextWs: "Roxana Diaz", FechaDesde: ""},
+    { Cargo: "(AUDITOR EN FORMACIÓN)", ParticipanteContextWs: "Monica Zuleta", FechaDesde: "" },
+  ];
 
   ngOnInit() {}
 
+  adicionarParticipante(){
+    this.visibleAdd = "SI";
+  }
+  cancelar(){
+    this.visibleAdd = "NO";
+  }
+  guardar(){
+    this.visibleAdd = "NO";
+  }
 }

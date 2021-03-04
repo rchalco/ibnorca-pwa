31408c6a7ef7895c96fa040f86a4f827041dba07
Ruben Dario Chalco Campos <nebur.dario@gmail.com>
2021-03-04 11:38:44 -0400

    ultima lista

--- ibnorca_ui/src/app/components/lista-verificacion-apertura/lista-verificacion-apertura.component.ts
+++ ibnorca_ui/src/app/components/lista-verificacion-apertura/lista-verificacion-apertura.component.ts
@@ -9,19 +9,43 @@ export class ListaVerificacionAperturaComponent implements OnInit {
 
   data = [
     {
-      name: 'primary',
+      name: 'Presentación del Equipo Auditor y de los representantes del auditado',
       selected: true
     },
     {
-      name: 'secondary',
+      name: 'Confirmar propósito y alcance de la auditoría',
       selected: false
     },
     {
-      name: 'tertiary',
+      name: 'Mencionar la(s) norma(s) de referencia de la auditoría para establecer la conformidad del producto',
       selected: true
     },
     {
-      name: 'success',
+      name: 'Hacer énfasis en el compromiso de confidencialidad',
+      selected: false
+    },
+    {
+      name: 'Definir canales de comunicación',
+      selected: false
+    },
+    {
+      name: 'Confirmar la disponibilidad de recursos y medios necesarios',
+      selected: false
+    },
+    {
+      name: 'Revisar el plan de auditoría y confirmar los horarios del cronograma, reunión de cierre y reuniones intermedias',
+      selected: false
+    },
+    {
+      name: 'Arreglos',
+      selected: false
+    },
+    {
+      name: 'Preguntas y respuestas',
+      selected: false
+    },
+    {
+      name: 'Agradecer a los participantes por asistir',
       selected: false
     },
   ];

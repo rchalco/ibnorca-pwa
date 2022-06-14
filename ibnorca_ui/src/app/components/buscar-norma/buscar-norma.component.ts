7cb87ee4f29a504ce7c14039ac459f424f602738
Ruben Dario Chalco Campos <nebur.dario@gmail.com>
2022-06-13 20:50:44 -0400

    correciones

--- ibnorca_ui/src/app/components/buscar-norma/buscar-norma.component.ts
+++ ibnorca_ui/src/app/components/buscar-norma/buscar-norma.component.ts
@@ -1,46 +1,55 @@
-import { Component, ComponentFactoryResolver, EventEmitter, OnInit, Output } from "@angular/core";
-import { Norma } from "src/app/interfaces/apertura_auditoria/Norma";
-import { AperturaAuditoriaService } from "src/app/services/apertura-auditoria.service";
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
+/* eslint-disable @typescript-eslint/member-ordering */
+import {
+  Component,
+  ComponentFactoryResolver,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Norma } from 'src/app/interfaces/apertura_auditoria/Norma';
+import { AperturaAuditoriaService } from 'src/app/services/apertura-auditoria.service';
 
 @Component({
-  selector: "app-buscar-norma",
-  templateUrl: "./buscar-norma.component.html",
-  styleUrls: ["./buscar-norma.component.scss"],
+  selector: 'app-buscar-norma',
+  templateUrl: './buscar-norma.component.html',
+  styleUrls: ['./buscar-norma.component.scss'],
 })
 export class BuscarNormaComponent implements OnInit {
   listaNormas: Norma[];
   currentNorma: Norma;
-  tipoNorma = "nacional";
+  tipoNorma = 'nacional';
   @Output() public selectNorma: EventEmitter<Norma> = new EventEmitter<Norma>();
 
   constructor(private aperturaAuditoriaService: AperturaAuditoriaService) {}
 
-    ngOnInit() {}
+  ngOnInit() {}
 
   buscar(event) {
-    let codigoNorma = event.detail.value;
+    const codigoNorma = event.detail.value;
     if (codigoNorma.length > 3) {
-      
-      if(this.tipoNorma == "nacional"){
-        console.log("buscamos la norma nacional");
-        this.aperturaAuditoriaService.BuscarNormas(codigoNorma).subscribe((x) => {
-          console.log(x);
-          this.listaNormas = x.listEntities;
-        });
+      if (this.tipoNorma == 'nacional') {
+        console.log('buscamos la norma nacional');
+        this.aperturaAuditoriaService
+          .BuscarNormas(codigoNorma)
+          .subscribe((x) => {
+            console.log(x);
+            this.listaNormas = x.listEntities;
+          });
+      } else {
+        console.log('buscamos la norma intenacional');
+        this.aperturaAuditoriaService
+          .BuscarNormasInternacionales(codigoNorma)
+          .subscribe((x) => {
+            console.log(x);
+            this.listaNormas = x.listEntities;
+          });
       }
-      else{
-        console.log("buscamos la norma intenacional");
-        this.aperturaAuditoriaService.BuscarNormasInternacionales(codigoNorma).subscribe((x) => {
-          console.log(x);
-          this.listaNormas = x.listEntities;
-        });
-      }
-      
     }
   }
   seleccionarNorma(event) {
-    console.log("seleccionar norma", event);
-    console.log("selectNorma",this.selectNorma);
+    console.log('seleccionar norma', event);
+    console.log('selectNorma', this.selectNorma);
     if (this.selectNorma) {
       this.selectNorma.emit(<Norma>event.detail.value);
     }
